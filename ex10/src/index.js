b0f878d98c1221757a438df8077eb91d6309fa1e
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-06 22:33:48 +0200

    Add Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,34 @@
+function myFunction(){
+var myMusic = {
+001 : {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP"
+    },
+    gold: true
+},
+
+002 :{
+    artist: "Artist",
+    title: "Album Title",
+    release_year: 2022,
+    formats: {
+        1: "OX",
+        2: "OY",
+        3: "OZ"
+    },
+    gold: true
+}
+};
+return myMusic;
+}
+
+myFunction()[2];
+console.log(myFunction());
+console.log(myFunction()[1]);
+console.log(myFunction()[2]);
+module.exports = myFunction;
